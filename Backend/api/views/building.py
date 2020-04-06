90a6b0c4c3efb192a9b8266bb0baf43b35341e1e
Mike <mike.awie@yahoo.com>
2020-04-06 13:40:09 +0300

    Costi get building method

--- Backend/api/views/building.py
+++ Backend/api/views/building.py
@@ -1,29 +1,64 @@
-import neomodel
-from flask import jsonify
 from flask_classful import FlaskView
-# se poate folosi si marshmallow ca se se creeze o schema pentru args
-from models import Building
+from flask import jsonify
 from webargs import fields
 from webargs.flaskparser import use_args
+from models import Building, Connector, Group, Waypoint, StudiesIn, Teacher, ClassRoom, Office
+import neomodel
 
 
 class BuildingView(FlaskView):
     base_args = ['args']
-    excluded_methods = []  # metode care nu corespund unei rute
+    excluded_methods = []
 
     # findAll
     def index(self):
         return jsonify(list(map(lambda building: building.__properties__, Building.nodes.all()))), 200
 
-    def get(self, uid):  # trebuie schimbat sa returneze toate datele despre o cladire/ eventual cautand dupa nume
-        building = Building.nodes.get_or_none(uid=uid)
+    def get(self, name):
+        building = Building.nodes.get_or_none(name=name)
         if building:
-            return jsonify(building.__properties__)
+            map = {"name": building.name, "uid": building.uid,
+                   "floors": [], "connectors": []}
+            for floor in building.floors:
+                map["floors"].append({"level": floor.level, "waypoints": []})
+                for waypoint in floor.waypoints:
+                    map["floors"][-1]["waypoints"].append(
+                        {"uid": waypoint.uid, "name": waypoint.name})
+                    if ClassRoom.__name__ in waypoint.labels():
+                        map["floors"][-1]["waypoints"][-1]["type"] = "classRoom"
+                        map["floors"][-1]["waypoints"][-1]["schedule"] = []
+                        for group in [group for group in Group.nodes.has(classes=True) if group.classes.relationship(waypoint)]:
+                            classes = group.classes.all_relationships(
+                                waypoint)
+                            for cclass in classes:
+                                map["floors"][-1]["waypoints"][-1]["schedule"].append(
+                                    {"group": group.name, "course": cclass.course, "dayOfWeek": cclass.dayOfWeek,
+                                     "startTime": cclass.startTime, "finishTime": cclass.finishTime})
+                    if Office.__name__ in waypoint.labels():
+                        map["floors"][-1]["waypoints"][-1]["type"] = "office"
+                        map["floors"][-1]["waypoints"][-1]["professors"] = []
+                        for teacher in Teacher.nodes.has(office=True):
+                            for office in teacher.office:
+                                if office.name == waypoint.name:
+                                    map["floors"][-1]["waypoints"][-1]["professors"].append(
+                                        teacher.name)
+                    if Connector.__name__ in waypoint.labels():
+                        map["floors"][-1]["waypoints"][-1]["type"] = "connector"
+                    map["floors"][-1]["waypoints"][-1]["neighbors"] = []
+                    for neighbor in waypoint.neighbors:
+                        map["floors"][-1]["waypoints"][-1]["neighbors"].append(
+                            {"name": neighbor.name})
+            for conn in Connector.nodes.all():
+                map["connectors"].append({"name": conn.name, "floors": []})
+                for floor in conn.floors:
+                    map["connectors"][-1]["floors"].append(floor.level)
+
+            return jsonify(map)
         else:
             return "Not Found", 404
 
     @use_args({"name": fields.Str(required=True)})
-    def post(self, args):  # trebuie schimbat sa primeasca toate datele si sa le salveze
+    def post(self, args):
         try:
             Building(name=args["name"]).save()
             return "Building created successfully", 200

1f764c982d5d993a7cae1de0f21b8689eae9752b
Octavian Iacob <octavian.iacob@icloud.com>
2020-04-12 18:15:07 +0300

    Adaugare lista "suggested places"

--- Proiect Android/Interfata/app/src/main/java/com/example/myapplication/StartNavigationActivity.java
+++ Proiect Android/Interfata/app/src/main/java/com/example/myapplication/StartNavigationActivity.java
@@ -1,9 +1,15 @@
 package com.example.myapplication;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,6 +17,9 @@ import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
 
 public class StartNavigationActivity extends AppCompatActivity {
@@ -26,8 +35,37 @@ public class StartNavigationActivity extends AppCompatActivity {
         mDrawerLayout.addDrawerListener(mToggle);
         mToggle.syncState();
         Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
-    }
+        final ListView lv = (ListView) findViewById(R.id.listView);
+        String[] fruits = new String[] {
+                "C112",
+                "C2",
+                "C403",
+                "C909",
+                "Exit" };
 
+        List<String> fruits_list = new ArrayList<String>(Arrays.asList(fruits));
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>
+                (this, android.R.layout.simple_list_item_1, fruits_list){
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent){
+                // Get the Item from ListView
+                View view = super.getView(position, convertView, parent);
+
+                // Initialize a TextView for ListView each Item
+                TextView tv = (TextView) view.findViewById(android.R.id.text1);
+
+                // Set the text color of TextView (ListView Item)
+                tv.setTextColor(Color.BLACK);
+                tv.setBackgroundColor(Color.rgb(243,237,218));
+
+                // Generate ListView Item using TextView
+                return view;
+            }
+    };
+        lv.setAdapter(arrayAdapter);
+
+
+    };
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         if(mToggle.onOptionsItemSelected(item)){
@@ -35,4 +73,5 @@ public class StartNavigationActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
 }

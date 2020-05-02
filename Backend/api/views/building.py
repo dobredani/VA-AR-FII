e15235eb8e2a170ee2902ebf692af6903caaf11e
Mike <mike.awie@yahoo.com>
2020-05-02 17:13:52 +0300

    validate waypoint type

--- Backend/api/views/building.py
+++ Backend/api/views/building.py
@@ -4,8 +4,9 @@ from webargs import fields, validate
 from webargs.flaskparser import use_args
 from models import Building, Floor, Waypoint, GoesTo, Connector, Group, Teacher, ClassRoom, Office
 from neomodel import db
-import sys
-import traceback
+
+WAYPOINT_TYPES = {"ClassRoom": "classRoom",
+                  "Office": "office", "Connector": "connector"}
 
 BuildingSchema = {
     'name': fields.Str(required=True),
@@ -14,7 +15,7 @@ BuildingSchema = {
         'waypoints': fields.List(fields.Nested({
             'name': fields.Str(required=True),
             'markerId': fields.Int(required=True),
-            'type': fields.Str(required=False),
+            'type': fields.Str(required=False, validate=validate.OneOf(list(WAYPOINT_TYPES.values()))),
             'schedule': fields.List(fields.Nested({
                 'group': fields.Str(required=True),
                 'course': fields.Str(required=True),
@@ -33,9 +34,6 @@ class BuildingView(FlaskView):
     base_args = ['args']
     excluded_methods = ['createBuilding']
 
-    WAYPOINT_TYPES = {"ClassRoom": "classRoom",
-                      "Office": "office", "Connector": "connector"}
-
     # findAll
     def index(self):
         return jsonify(list(map(lambda building: building.name, Building.nodes.all()))), 200
@@ -53,7 +51,7 @@ class BuildingView(FlaskView):
                         map["floors"][-1]["waypoints"].append(
                             {"name": waypoint.name, "markerId": waypoint.markerId})
                         if ClassRoom.__name__ in waypoint.labels():
-                            map["floors"][-1]["waypoints"][-1]["type"] = self.WAYPOINT_TYPES["ClassRoom"]
+                            map["floors"][-1]["waypoints"][-1]["type"] = WAYPOINT_TYPES["ClassRoom"]
                             map["floors"][-1]["waypoints"][-1]["schedule"] = []
                             for group in [group for group in Group.nodes.filter(buildingName=name).has(classes=True) if group.classes.relationship(waypoint)]:
                                 classes = group.classes.all_relationships(
@@ -63,13 +61,13 @@ class BuildingView(FlaskView):
                                         {"group": group.name, "course": cclass.course, "dayOfWeek": cclass.dayOfWeek,
                                          "startTime": cclass.startTime, "finishTime": cclass.finishTime})
                         elif Office.__name__ in waypoint.labels():
-                            map["floors"][-1]["waypoints"][-1]["type"] = self.WAYPOINT_TYPES["Office"]
+                            map["floors"][-1]["waypoints"][-1]["type"] = WAYPOINT_TYPES["Office"]
                             map["floors"][-1]["waypoints"][-1]["professors"] = []
                             for teacher in [teacher for teacher in Teacher.nodes.filter(buildingName=name).has(office=True) if teacher.office.relationship(waypoint)]:
                                 map["floors"][-1]["waypoints"][-1]["professors"].append(
                                     teacher.name)
                         elif Connector.__name__ in waypoint.labels():
-                            map["floors"][-1]["waypoints"][-1]["type"] = self.WAYPOINT_TYPES["Connector"]
+                            map["floors"][-1]["waypoints"][-1]["type"] = WAYPOINT_TYPES["Connector"]
 
                         map["floors"][-1]["waypoints"][-1]["neighbors"] = []
                         for neighbor in waypoint.neighbors:
@@ -94,7 +92,7 @@ class BuildingView(FlaskView):
                       buildingName=building["name"]).save()
                 for waypoint in floor["waypoints"]:
                     if ("type" in waypoint):
-                        if (waypoint["type"] == self.WAYPOINT_TYPES["ClassRoom"]):
+                        if (waypoint["type"] == WAYPOINT_TYPES["ClassRoom"]):
                             classRoom = ClassRoom(
                                 name=waypoint["name"], markerId=waypoint["markerId"], buildingName=building["name"], floorLevel=floor["level"]).save()
                             for schedule in waypoint["schedule"]:
@@ -104,14 +102,14 @@ class BuildingView(FlaskView):
                                                                   'dayOfWeek': schedule["dayOfWeek"],
                                                                   'startTime': schedule["startTime"],
                                                                   'finishTime': schedule["finishTime"]})
-                        elif (waypoint["type"] == self.WAYPOINT_TYPES["Office"]):
+                        elif (waypoint["type"] == WAYPOINT_TYPES["Office"]):
                             office = Office(
                                 name=waypoint["name"], markerId=waypoint["markerId"], buildingName=building["name"], floorLevel=floor["level"]).save()
                             for prof in waypoint["professors"]:
                                 teacher = Teacher(
                                     name=prof, buildingName=building["name"]).save()
                                 teacher.office.connect(office)
-                        elif (waypoint["type"] == self.WAYPOINT_TYPES["Connector"]):
+                        elif (waypoint["type"] == WAYPOINT_TYPES["Connector"]):
                             Connector(
                                 name=waypoint["name"], markerId=waypoint["markerId"], buildingName=building["name"], floorLevel=floor["level"]).save()
                         else:
@@ -133,7 +131,6 @@ class BuildingView(FlaskView):
             db.commit()
             return self.get(building["name"]), 200
         except Exception as e:
-            traceback.print_exc(file=sys.stdout)
             db.rollback()
             return str(e), 500
 
